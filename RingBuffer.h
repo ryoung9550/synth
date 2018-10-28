068be8542ab09524635195c5618a52197ff24662
Ryan Young <ryanyoung1@hotmail.com>
2018-10-28 12:35:04 -0400

    Resolved more crackling

--- RingBuffer.h
+++ RingBuffer.h
@@ -10,15 +10,15 @@ class RingBuffer
 	int readIndex = 0;
 	T data[size] {0};
 	unsigned WRdiff = 0;
-	const int BUFFPADDING = 8000;
+	const int BUFFPADDING = 1000;
 public:
 	T read()
 	{
 		if(readIndex >= size) {
 			readIndex = 0;
 		}
-		if(WRdiff <= BUFFPADDING) {
-			return 0;
+		while(WRdiff <= 0) {
+			//return 0;
 		}
 		WRdiff--;
 		return data[readIndex++];
@@ -28,7 +28,7 @@ public:
 		if(writeIndex >= size) {
 			writeIndex = 0;
 		}
-		while(WRdiff >= size - BUFFPADDING /2);
+		while(WRdiff >= size - BUFFPADDING);
 		WRdiff++;
 		data[writeIndex++] = _data;
 		return 0;
