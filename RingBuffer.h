609ed845549ef24856d837ea0fa1dc9b3087b297
Ryan Young <ryanyoung1@hotmail.com>
2018-10-29 02:39:49 -0400

    Fixed random crackling

--- RingBuffer.h
+++ RingBuffer.h
@@ -2,6 +2,7 @@
 #define _RING_BUFFER_H
 
 #include <cstring>
+#include <mutex>
 
 template<typename T, int size>
 class RingBuffer
@@ -9,18 +10,20 @@ class RingBuffer
 	int writeIndex = 0;
 	int readIndex = 0;
 	T data[size] {0};
+	std::mutex mtx;
 	unsigned WRdiff = 0;
-	const int BUFFPADDING = 3000;
 public:
 	T read()
 	{
 		if(readIndex >= size) {
 			readIndex = 0;
 		}
-		while(WRdiff <= BUFFPADDING) {
+		while(WRdiff <= 0) {
 			//return 0;
 		}
+		mtx.lock();
 		WRdiff--;
+		mtx.unlock();
 		return data[readIndex++];
 	}
 	int write(T _data)
@@ -28,8 +31,10 @@ public:
 		if(writeIndex >= size) {
 			writeIndex = 0;
 		}
-		while(WRdiff >= size - BUFFPADDING);
+		while(WRdiff >= size - 1);
+		mtx.lock();
 		WRdiff++;
+		mtx.unlock();
 		data[writeIndex++] = _data;
 		return 0;
 	}
